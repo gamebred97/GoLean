9a7a777e2c0ea8d92610f6fc7c6e9333c4c5beb0
gamebred97 <krasiikostdainov7@gmail.com>
2025-05-22 13:03:16 +0300

    adding remove total

--- Sports/src/Total/Total.jsx
+++ Sports/src/Total/Total.jsx
@@ -1,16 +1,15 @@
-import { Box, Text, useSegmentGroup } from "@chakra-ui/react";
+import { Box, Button, Text, useSegmentGroup } from "@chakra-ui/react";
 import { getUserPathByUid } from "../services/users.service";
 import { AppContext } from "../state/app.context";
 import { useContext } from "react";
 import { db } from "../config/firebase-config";
-import { set, update, ref, get, push } from "firebase/database";
+import { set, update, ref, get, push, remove } from "firebase/database";
 import { useEffect, useState } from "react";
 
 function Total({ intake }) {
+  const { user } = useContext(AppContext);
 
-  const {user} = useContext(AppContext)
-
-  const [total, setTotal] = useState(null)
+  const [total, setTotal] = useState(null);
 
   const totalCalories = intake.reduce((sum, item) => sum + item.nf_calories, 0);
   const totalProtein = intake.reduce((sum, item) => sum + item.nf_protein, 0);
@@ -20,73 +19,104 @@ function Total({ intake }) {
   );
   const totalFat = intake.reduce((sum, item) => sum + item.nf_total_fat, 0);
 
-
   async function getTotalDB() {
-    if(!user) return;
-    const uniqueUser = await getUserPathByUid(user.uid)
-    if(!uniqueUser) return;
+    if (!user) return;
+    const uniqueUser = await getUserPathByUid(user.uid);
+    if (!uniqueUser) return;
 
-    const path = ref(db, `${uniqueUser}/total`)
+    const path = ref(db, `${uniqueUser}/total`);
 
     try {
-      const snapshot = await get(path)
-      if(snapshot.exists()){
-        setTotal(snapshot.val())
+      const snapshot = await get(path);
+      if (snapshot.exists()) {
+        setTotal(snapshot.val());
       } else {
-        setTotal(null)
+        setTotal(null);
       }
     } catch (error) {
       console.error("Error while creating total", error);
     }
   }
 
-
   async function setTotalDB() {
-    
-    if(!user) return;
+    if (!user) return;
 
-    const uniqueUser = await getUserPathByUid(user.uid)
+    const uniqueUser = await getUserPathByUid(user.uid);
 
-    if(!uniqueUser) return;
+    if (!uniqueUser) return;
 
-    const path = ref(db, `${uniqueUser}/total`)
+    const path = ref(db, `${uniqueUser}/total`);
 
     const newTotal = {
       calories: totalCalories,
       protein: totalProtein,
       carbs: totalCarbs,
       fat: totalFat,
-    }
+    };
 
     try {
-      const newRef = await set(path, newTotal)
-      return newRef.key
+      const newRef = await set(path, newTotal);
+      return newRef.key;
     } catch (error) {
       console.error("Error while creating total", error);
     }
   }
 
-  
+  async function resetTotal() {
+    if (!user) return;
+    const uniqueUser = await getUserPathByUid(user.uid);
 
-  useEffect(() => {
-    if (intake.length > 0){
-      setTotalDB()
+    if (!uniqueUser) return;
+
+    const path = ref(db, `${uniqueUser}/total`);
+
+    const zeroTotal = {
+    calories: 0,
+    protein: 0,
+    carbs: 0,
+    fat: 0,
+  };
+
+    try {
+      await set(path, zeroTotal)
+      setTotal(null)
+      
+    } catch (error) {
+      console.error("Error while re-setting total", error);
     }
-  },[intake])
+  }
 
   useEffect(() => {
-    getTotalDB()
-  },[user])
+    if (intake.length > 0) {
+      setTotalDB();
+    }
+  }, [intake]);
+
+  useEffect(() => {
+    getTotalDB();
+  }, [user]);
 
   return (
     <>
       {(intake.length > 0 || total) && (
         <Box mt={4} p={4} bg="gray.900" borderRadius="md">
           <Text fontWeight="bold">Macros:</Text>
-          <Text>Calories: {(intake.length > 0 ? totalCalories : total?.calories)?.toFixed(1)}</Text>
-          <Text>Protein: {(intake.length > 0 ? totalProtein : total?.protein)?.toFixed(1)}g</Text>
-          <Text>Carbs: {(intake.length > 0 ? totalCarbs : total?.carbs)?.toFixed(1)}g</Text>
-          <Text>Fat: {(intake.length > 0 ? totalFat : total?.fat)?.toFixed(1)}g</Text>
+          <Text>
+            Calories:{" "}
+            {(intake.length > 0 ? totalCalories : total?.calories)?.toFixed(1)}
+          </Text>
+          <Text>
+            Protein:{" "}
+            {(intake.length > 0 ? totalProtein : total?.protein)?.toFixed(1)}g
+          </Text>
+          <Text>
+            Carbs: {(intake.length > 0 ? totalCarbs : total?.carbs)?.toFixed(1)}
+            g
+          </Text>
+          <Text>
+            Fat: {(intake.length > 0 ? totalFat : total?.fat)?.toFixed(1)}g
+          </Text>
+          <Button onClick={resetTotal}>Clear</Button>
         </Box>
       )}
     </>
